190ebb4cdd50bd640bb8b428523f01a0a0254c78
Leo <leonardo8571@gmail.com>
2025-11-10 23:39:37 -0300

    index

---
+++ public/index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <?php require "head.php" ?>
+</head>
+
+<body>
+
+    <?php require "menu.php" ?>
+
+    <h1>Trabalho Web II</h1>
+
+    O trabalho tem o objetivo de aplicar na prática os conceitos estudados na
+    disciplina **Web II** em uma aplicação que envolva a utilização de *JavaScript, jQuery e
+    PHP/MySQL.*
+    <ol>
+        <li><label><input checked disabled type="checkbox"><b>Uma tabela de <i>Banco de Dados</i>.</b></label>
+            <ul>
+                <li><label><input checked disabled type="checkbox">Com <b>6 ou mais</b> atributos (colunas).</label></li>
+                <li><label><input checked disabled type="checkbox">Com <b>4 ou mais</b> tipos diferentes de dados.</label></li>
+                <li><label><input checked disabled type="checkbox">Deve ser <b>diferente</b> da tabela usada nos materiais da disciplina.</label></li>
+            </ul>
+        </li>
+
+        <li><label><input checked disabled type="checkbox"><b>Página para exibir os dados da tabela.</b></label>
+            <ul>
+                <li><label><input checked disabled type="checkbox">Com opção para <b>Inserir</b> dados.</label></li>
+                <li><label><input disabled type="checkbox">Com opção para <b>Excluir</b> dados.</label></li>
+                <li><label><input disabled type="checkbox">Com opção para <b>Alterar</b> dados.</label></li>
+            </ul>
+        </li>
+
+        <li><label><input checked disabled type="checkbox"><b>Formulário HTML para inserir dados.</b></label>
+            <ul>
+                <li><label><input checked disabled type="checkbox"> Usando <b>tipos adequados.</b></label></li>
+                <li><label><input disabled type="checkbox"> <b>Formatação</b> com <i>CSS/Bootstrap/etc.</i></label></li>
+                <li><label><input disabled type="checkbox"> <i>JavaScript</i> funcional para <b>validação.</b></label></li>
+                <li><label><input checked disabled type="checkbox"> <i>PHP</i> para <b>Inserir</b> dados no <i>BD</i>.</label></li>
+            </ul>
+        </li>
+
+        <li><label><input disabled type="checkbox"><b>Página para Excluir Dados.</b></label></li>
+
+        <li><label><input disabled type="checkbox"><b>Página para Alteração de Dados.</b></label>
+            <ul>
+                <li><label><input disabled type="checkbox" name="" id="">Com as mesmas <b>validações</b> da <i>inserção</i></label></li>
+            </ul>
+        </li>
+    </ol>
+
+</body>
+
+</html>
