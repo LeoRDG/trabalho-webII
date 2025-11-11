23a5f7b348adb85182c7c6fd0660c24c5d495527
Leo <leonardo8571@gmail.com>
2025-11-11 01:33:58 -0300

    "modulos"

---
+++ public/menu.php
@@ -0,0 +1,5 @@
+<ul class="menu">
+    <li><a href="index.php">Home</a></li>
+    <li><a href="ver_produtos.php">Produtos</a></li>
+    <li><a href="adicionarproduto.php">Adicionar Produto</a></li>
+</ul>
