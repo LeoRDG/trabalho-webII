1c910a5629de87a0392795032b1428e92857c462
Leo <leonardo8571@gmail.com>
2025-11-16 00:46:26 -0300

    ajustes Bd

--- src/banco.sql
+++ src/banco.sql
@@ -2,17 +2,17 @@ CREATE DATABASE `web2`;
 USE DATABASE `web2`;
 
 CREATE TABLE `produtos` (
-	`ìd` INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
-    `nome`VARCHAR(60) NOT NULL,
+	`id` INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    `nome`VARCHAR(100) NOT NULL,
     `marca` VARCHAR(40),
     `categoria` VARCHAR(40),
     `descricao` TEXT,
-    `preco` DECIMAL NOT NULL,
+    `preco` DECIMAL(10,2) NOT NULL,
     `estoque` INT NOT NULL DEFAULT 0,
-    `peso` DECIMAL NOT NULL,
+    `peso` DECIMAL(10,2) NOT NULL,
     `condicao` ENUM('Novo','Usado','Recondicionado') NOT NULL,
     `frete_gratis` TINYINT NOT NULL DEFAULT 0,
-    `caminho_imagem` VARCHAR(100),
-    `criado_em` DATE NOT NULL,
-    `modificado_em`TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+    `imagem` VARCHAR(100),
+    `criado_em` DATE NOT NULL DEFAULT (CURDATE()),
+    `modificado_em` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
