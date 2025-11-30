f633b759d9fab004bd4789aa5d7451259b841499
Leo <leonardo8571@gmail.com>
2025-11-30 16:21:42 -0300

    Adicionando informacoes ao readme e index

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Trabalho de Desenvolvimento Web II
+# [Trabalho de Desenvolvimento Web II](https://github.com/LeoRDG/trabalho-webii)
 
 O trabalho tem o objetivo de aplicar na prática os conceitos estudados na
 disciplina **Web II** em uma aplicação que envolva a utilização de *JavaScript, jQuery e
@@ -26,3 +26,40 @@ PHP/MySQL.*
 
 - [x] **5. Página para Alteração de Dados.**
    - [x] Com as mesmas **validações** da *inserção*
+
+***
+
+### Estrutura do Banco de Dados
+
+A tabela `produtos` possui os seguintes atributos:
+
+- **id** (INT, AUTO_INCREMENT, PRIMARY KEY): Identificador único do produto
+- **nome** (VARCHAR(60)): Nome do produto
+- **marca** (VARCHAR(20)): Marca do produto
+- **categoria** (VARCHAR(30)): Categoria do produto
+- **descricao** (TEXT): Descrição detalhada do produto
+- **preco** (DECIMAL(8,2)): Preço do produto (entre R$ 1,00 e R$ 100.000,00)
+- **estoque** (SMALLINT UNSIGNED): Quantidade em estoque (0 a 5000)
+- **peso** (DECIMAL(3,1)): Peso do produto em kg (0,1 a 50,0)
+- **vencimento** (DATE): Data de vencimento do produto
+- **condicao** (ENUM): Estado do produto (Novo, Usado, Recondicionado)
+- **frete_gratis** (BOOLEAN): Indica se o produto possui frete grátis
+- **criado_em** (DATETIME): Data e hora de criação do registro
+- **modificado_em** (TIMESTAMP): Data e hora da última modificação
+
+***
+
+### Estrutura do Projeto
+```
+trabalho-webII/
+├── banco.sql              # Script de criação do banco de dados
+├── index.php              # Página inicial
+├── css/                   # Arquivos de estilização
+├── js/                    # Scripts JavaScript e jQuery
+├── php/
+│   ├── actions/           # Arquivos de CRUD
+│   ├── include/           # Componentes reutilizáveis
+│   ├── paginas/           # Páginas acessíveis
+│   └── src/               # Classes PHP
+└── README.md              
+```
--- php/paginas/index.php
+++ php/paginas/index.php
@@ -15,46 +15,85 @@
     ?>
 
     <main>
-        <h1>Trabalho Web II</h1>
+        <h1><a target="_blank" href="https://github.com/LeoRDG/trabalho-webii">Trabalho de Desenvolvimento Web II</a></h1>
 
         <p>O trabalho tem o objetivo de aplicar na prática os conceitos estudados na
         disciplina <b>Web II</b> em uma aplicação que envolva a utilização de <i>JavaScript, jQuery e
         PHP/MySQL.</i></p>
 
-        <ol>
-            <li><span class="material-symbols-outlined">check</span><b>Uma tabela de <i>Banco de Dados</i>.</b>
-                <ul>
-                    <li><span class="material-symbols-outlined">check</span>Com <b>6 ou mais</b> atributos (colunas).</li>
-                    <li><span class="material-symbols-outlined">check</span>Com <b>4 ou mais</b> tipos diferentes de dados.</li>
-                    <li><span class="material-symbols-outlined">check</span>Deve ser <b>diferente</b> da tabela usada nos materiais da disciplina.</li>
-                </ul>
-            </li>
+        <h2>Requisitos</h2>
+        <div>
+            <ol>
+                <li><span class="material-symbols-outlined">check</span><b>Uma tabela de <i>Banco de Dados</i>.</b>
+                    <ul>
+                        <li><span class="material-symbols-outlined">check</span>Com <b>6 ou mais</b> atributos (colunas).</li>
+                        <li><span class="material-symbols-outlined">check</span>Com <b>4 ou mais</b> tipos diferentes de dados.</li>
+                        <li><span class="material-symbols-outlined">check</span>Deve ser <b>diferente</b> da tabela usada nos materiais da disciplina.</li>
+                    </ul>
+                </li>
 
-            <li><span class="material-symbols-outlined">check</span><b>Página para exibir os dados da tabela.</b>
-                <ul>
-                    <li><span class="material-symbols-outlined">check</span>Com opção para <b>Inserir</b> dados.</li>
-                    <li><span class="material-symbols-outlined">check</span>Com opção para <b>Excluir</b> dados.</li>
-                    <li><span class="material-symbols-outlined">check</span>Com opção para <b>Alterar</b> dados.</li>
-                </ul>
-            </li>
+                <li><span class="material-symbols-outlined">check</span><b>Página para exibir os dados da tabela.</b>
+                    <ul>
+                        <li><span class="material-symbols-outlined">check</span>Com opção para <b>Inserir</b> dados.</li>
+                        <li><span class="material-symbols-outlined">check</span>Com opção para <b>Excluir</b> dados.</li>
+                        <li><span class="material-symbols-outlined">check</span>Com opção para <b>Alterar</b> dados.</li>
+                    </ul>
+                </li>
 
-            <li><span class="material-symbols-outlined">check</span><b>Formulário HTML para inserir dados.</b>
-                <ul>
-                    <li><span class="material-symbols-outlined">check</span> Usando <b>tipos adequados.</b></li>
-                    <li><span class="material-symbols-outlined">check</span> <b>Formatação</b> com <i>CSS/Bootstrap/etc.</i></li>
-                    <li><span class="material-symbols-outlined">check</span> <i>JavaScript</i> funcional para <b>validação.</b></li>
-                    <li><span class="material-symbols-outlined">check</span> <i>PHP</i> para <b>Inserir</b> dados no <i>BD</i>.</li>
-                </ul>
-            </li>
+                <li><span class="material-symbols-outlined">check</span><b>Formulário HTML para inserir dados.</b>
+                    <ul>
+                        <li><span class="material-symbols-outlined">check</span> Usando <b>tipos adequados.</b></li>
+                        <li><span class="material-symbols-outlined">check</span> <b>Formatação</b> com <i>CSS/<s>Bootstrap/etc.</s></i></li>
+                        <li><span class="material-symbols-outlined">check</span> <i>JavaScript</i> funcional para <b>validação.</b></li>
+                        <li><span class="material-symbols-outlined">check</span> <i>PHP</i> para <b>Inserir</b> dados no <i>BD</i>.</li>
+                    </ul>
+                </li>
 
-            <li><span class="material-symbols-outlined">check</span><b>Página para Excluir Dados.</b></li>
+                <li><span class="material-symbols-outlined">check</span><b>Página para Excluir Dados.</b></li>
 
-            <li><span class="material-symbols-outlined">check</span><b>Página para Alteração de Dados.</b>
-                <ul>
-                    <li><span class="material-symbols-outlined">check</span>Com as mesmas <b>validações</b> da <i>inserção</i></li>
-                </ul>
-            </li>
-        </ol>
+                <li><span class="material-symbols-outlined">check</span><b>Página para Alteração de Dados.</b>
+                    <ul>
+                        <li><span class="material-symbols-outlined">check</span>Com as mesmas <b>validações</b> da <i>inserção</i></li>
+                    </ul>
+                </li>
+            </ol>
+        </div>
+
+        <h2>Estrutura do Banco de Dados</h2>
+        <div>
+            <p>A tabela <code>produtos</code> possui os seguintes atributos:</p>
+            <ul>
+                <li><b>id</b> (INT, AUTO_INCREMENT, PRIMARY KEY): Identificador único do produto</li>
+                <li><b>nome</b> (VARCHAR(60)): Nome do produto</li>
+                <li><b>marca</b> (VARCHAR(20)): Marca do produto</li>
+                <li><b>categoria</b> (VARCHAR(30)): Categoria do produto</li>
+                <li><b>descricao</b> (TEXT): Descrição detalhada do produto</li>
+                <li><b>preco</b> (DECIMAL(8,2)): Preço do produto (entre R$ 1,00 e R$ 100.000,00)</li>
+                <li><b>estoque</b> (SMALLINT UNSIGNED): Quantidade em estoque (0 a 5000)</li>
+                <li><b>peso</b> (DECIMAL(3,1)): Peso do produto em kg (0,1 a 50,0)</li>
+                <li><b>vencimento</b> (DATE): Data de vencimento do produto</li>
+                <li><b>condicao</b> (ENUM): Estado do produto (Novo, Usado, Recondicionado)</li>
+                <li><b>frete_gratis</b> (BOOLEAN): Indica se o produto possui frete grátis</li>
+                <li><b>criado_em</b> (DATETIME): Data e hora de criação do registro</li>
+                <li><b>modificado_em</b> (TIMESTAMP): Data e hora da última modificação</li>
+            </ul>
+        </div>
+
+        <h2>Estrutura do Projeto</h2>
+
+        <div>
+            <pre class="code">trabalho-webII/
+├── banco.sql              # Script de criação do banco de dados
+├── index.php              # Página inicial
+├── css/                   # Arquivos de estilização
+├── js/                    # Scripts JavaScript e jQuery
+├── php/
+│   ├── actions/           # Arquivos de CRUD
+│   ├── include/           # Componentes reutilizáveis
+│   ├── paginas/           # Páginas acessíveis
+│   └── src/               # Classes PHP
+└── README.md</pre>
+        </div>
     </main>
 </body>
 
